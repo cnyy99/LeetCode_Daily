071a1cffb4a1a411124b935aa525c5f4d9a147ed
cnyy <chennansoul@gmail.com>
2019-04-15 16:38:31 +0800

    add [167] Two Sum II - Input array is sorted

---
+++ src/EASY/167.two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,49 @@
+#
+# @lc app=leetcode id=167 lang=python3
+#
+# [167] Two Sum II - Input array is sorted
+#
+# https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
+#
+# algorithms
+# Easy (49.44%)
+# Total Accepted:    230.2K
+# Total Submissions: 461.7K
+# Testcase Example:  '[2,7,11,15]\n9'
+#
+# Given an array of integers that is already sorted in ascending order, find
+# two numbers such that they add up to a specific target number.
+# 
+# The function twoSum should return indices of the two numbers such that they
+# add up to the target, where index1 must be less than index2.
+# 
+# Note:
+# 
+# 
+# Your returned answers (both index1 and index2) are not zero-based.
+# You may assume that each input would have exactly one solution and you may
+# not use the same element twice.
+# 
+# 
+# Example:
+# 
+# 
+# Input: numbers = [2,7,11,15], target = 9
+# Output: [1,2]
+# Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
+# 
+#
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        i=0
+        j=len(numbers)-1
+        sol=[0,0]
+        while i!=j:
+            if numbers[i]+numbers[j]==target:
+                sol[0]=i+1;
+                sol[1]=j+1;
+                return sol;
+            if numbers[i]+numbers[j]>target:
+                j-=1
+            else:
+                i+=1
