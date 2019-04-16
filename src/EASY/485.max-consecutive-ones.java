6684d7eee70799fe5b6e296de699f5fe13102965
cnyy <chennansoul@gmail.com>
2019-04-16 09:16:36 +0800

    add  [485] Max Consecutive Ones

---
+++ src/EASY/485.max-consecutive-ones.java
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode id=485 lang=java
+ *
+ * [485] Max Consecutive Ones
+ *
+ * https://leetcode.com/problems/max-consecutive-ones/description/
+ *
+ * algorithms
+ * Easy (54.54%)
+ * Total Accepted:    130.8K
+ * Total Submissions: 238.8K
+ * Testcase Example:  '[1,0,1,1,0,1]'
+ *
+ * Given a binary array, find the maximum number of consecutive 1s in this
+ * array.
+ * 
+ * Example 1:
+ * 
+ * Input: [1,1,0,1,1,1]
+ * Output: 3
+ * Explanation: The first two digits or the last three digits are consecutive
+ * 1s.
+ * ⁠   The maximum number of consecutive 1s is 3.
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * The input array will only contain 0 and 1.
+ * The length of input array is a positive integer and will not exceed 10,000
+ * 
+ * 
+ */
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int m=0,max=0;
+        for(int it=0;it<nums.length;it++)
+        {
+            if(nums[it]==1)
+            {
+                m++;
+                max=max>m?max:m;
+            }
+            else
+            {
+                m=0;
+            }
+        }
+        return max;
+    }
+}
+
---
+++ src/EASY/485.max-consecutive-ones.py
@@ -0,0 +1,45 @@
+#
+# @lc app=leetcode id=485 lang=python3
+#
+# [485] Max Consecutive Ones
+#
+# https://leetcode.com/problems/max-consecutive-ones/description/
+#
+# algorithms
+# Easy (54.54%)
+# Total Accepted:    130.8K
+# Total Submissions: 238.8K
+# Testcase Example:  '[1,0,1,1,0,1]'
+#
+# Given a binary array, find the maximum number of consecutive 1s in this
+# array.
+# 
+# Example 1:
+# 
+# Input: [1,1,0,1,1,1]
+# Output: 3
+# Explanation: The first two digits or the last three digits are consecutive
+# 1s.
+# ⁠   The maximum number of consecutive 1s is 3.
+# 
+# 
+# 
+# Note:
+# 
+# The input array will only contain 0 and 1.
+# The length of input array is a positive integer and will not exceed 10,000
+# 
+# 
+#
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        i=0
+        m=0
+        for it in nums:
+            if it==1:
+                i+=1
+                m=m if m>i else i
+            else:
+                i=0
+        return m
+
