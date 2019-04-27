23fff19c520a6f135f9999401aa069a3ed824c50
cnyy <chennansoul@gmail.com>
2019-04-27 23:05:36 +0800

    add  [78] Subsets

---
+++ leetcode-algorithms/078. Subsets/78.subsets.java
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode id=78 lang=java
+ *
+ * [78] Subsets
+ *
+ * https://leetcode.com/problems/subsets/description/
+ *
+ * algorithms
+ * Medium (51.15%)
+ * Total Accepted:    354K
+ * Total Submissions: 677.8K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * Given a set of distinct integers, nums, return all possible subsets (the
+ * power set).
+ * 
+ * Note: The solution set must not contain duplicate subsets.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: nums = [1,2,3]
+ * Output:
+ * [
+ * ⁠ [3],
+ * [1],
+ * [2],
+ * [1,2,3],
+ * [1,3],
+ * [2,3],
+ * [1,2],
+ * []
+ * ]
+ * 
+ */
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> sol = new ArrayList<>();
+        sol.add(new ArrayList<>());
+        for(int n : nums){
+            int size = sol.size();
+            for(int i=0; i<size; i++){
+                List<Integer> subset = new ArrayList<>(sol.get(i));
+                subset.add(n);
+                sol.add(subset);
+            }
+        }
+        return sol;
+    }
+}
+
