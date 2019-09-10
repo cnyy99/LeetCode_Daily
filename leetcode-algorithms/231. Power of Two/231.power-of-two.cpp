7137d8cf4af3886e0b7194908a0d1b2bc96b69f5
chennan <chennansoul@gamil.com>
2019-09-10 11:32:39 +0800

    format files

--- leetcode-algorithms/231. Power of Two/231.power-of-two.cpp
+++ leetcode-algorithms/231. Power of Two/231.power-of-two.cpp
@@ -37,9 +37,11 @@
  * Output: false
  * 
  */
-class Solution {
+class Solution
+{
 public:
-    bool isPowerOfTwo(int n) {
+    bool isPowerOfTwo(int n)
+    {
         return n > 0 && !(n & (n - 1));
     }
 };
